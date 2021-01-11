cc624270aefc30e52107d1027d03b3d30bcd2f17
jams10 <52204522+jams10@users.noreply.github.com>
2021-01-11 21:11:26 +0900

    Function Parameters and Typing

---
+++ Function_Params_and_Typing.py
@@ -0,0 +1,8 @@
+def addition(x,y):
+    return x+y
+
+z = addition("hey", " there")
+
+print(z)
+
+# 파이썬은 동적 타이핑(dynamic typing) 언어임. 프로그램 실행 시에 type check를 한다는 뜻.
--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -2,15 +2,19 @@ game = [[0, 0, 0],
         [0, 0, 0],
         [0, 0, 0],]
 
-def game_board():
+def game_board(player = 0, row = 0, column = 0, just_display = False):
         print("   0  1  2 ")
+        if not just_display:
+                game[row][column] = player
         # enumerate : 반복문 사용 시 몇 번째 반복문인지 확인이 필요할 때 사용.
         # 인덱스 번호와 컬렉션의 원소를 tuple 형태로 반환.  
         for count, row in enumerate(game):
                 print(count, row)
 
-x = game_board # x가 game_board() 함수가 됨.
+b = game_board # x가 game_board() 함수가 됨.
 
-game[0][1] = 1
+b(just_display = True)
+b(player = 1, row = 2, column = 0)
 
-x()
+
+
